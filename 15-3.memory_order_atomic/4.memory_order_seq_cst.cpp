8afa918286d3c4f206df21a6109d14c6846bf951
kenloumixx <kenloumixx@gmail.com>
2024-12-06 18:28:13 +0900

    feat: use memory_order_seq_cost for guaranteed order for every thread
    - "순차적 일관성" : 해당 명령을 사용하는 메모리 연산들끼리는 모든 스레드에서 동일한 연산 순서 관찰 보장됨.
    - atomic 객체 사용시 memory order의 default값
    - 인텔, AMD의 CPU는 ㅅ군차적 일관성이 보장돼서 비용 크지는 않지만 ARM은 비용 크므로 필요할때만!

---
+++ 15-3.memory_order_atomic/4.memory_order_seq_cst.cpp
@@ -0,0 +1,59 @@
+#include <atomic>
+#include <vector>
+#include <thread>
+#include <iostream>
+
+std::atomic<bool> x(false);
+std::atomic<bool> y(false);
+std::atomic<int> z(0);
+
+void write_x() 
+{
+    x.store(true, std::memory_order_seq_cst);
+}
+
+void write_y() 
+{
+    y.store(true, std::memory_order_seq_cst);
+}
+
+void read_x_then_y()
+{
+    while (!x.load(std::memory_order_seq_cst))
+    {
+
+    }
+    if (y.load(std::memory_order_seq_cst))
+    {
+        std::cout << "X : " << x << " and Y : " << y << std::endl;
+        ++z;
+    }
+}
+
+void read_y_then_x()
+{
+    while (!y.load(std::memory_order_seq_cst))
+    {
+
+    }
+    if (x.load(std::memory_order_seq_cst))
+    {
+        std::cout << "X : " << x << " and Y : " << y << std::endl;
+        ++z;
+    }
+}
+
+int main()
+{
+    std::thread a(write_x);
+    std::thread b(write_y);
+    std::thread c(read_x_then_y);
+    std::thread d(read_y_then_x);
+
+    a.join();
+    b.join();
+    c.join();
+    d.join();
+    std::cout << "Z : " << z << std::endl; 
+    return 0;
+}
